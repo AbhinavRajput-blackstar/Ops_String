e3a52d218dc4e2ff21e9068cea547fd45f279a40
AbhinavRajput-blackstar <54723245+AbhinavRajput-blackstar@users.noreply.github.com>
2019-10-23 00:11:47 +0530

    Update splitstring.c

--- splitstring.c
+++ splitstring.c
@@ -1,36 +1,58 @@
 #include<stdio.h>
 #include<string.h>
-     main()
-    {
-      char s[20],a,b[20],d[20];
-      int c,i,j=0,k=0,m;
-      printf("Enter String: ");
-      gets(s); 
-      printf("Split from:  ");
-    scanf("%c",&a);
-      c=strlen(s);
-
-    for(i=0;i<=c;i++)
-      {
-        if(s[i]!=a)
-        {
-          b[i]=s[i];
-        }	
-        else if(s[i]==a)
-        {
-          j=i;
-          }	
-      }
-      for(i=j;i<=c;i++)
-        {
-
-          d[k]=s[i];
-          k++;
-
-    }
-         printf("String before %c is : ",a);
-        puts(b);
-        printf("String after and including %c is : ",a);
-        puts(d);
-
-    }
+char split_str(char *a,char b)
+{while(*a!=b)
+ {  printf("%c",*a);
+    a++;
+ }
+ printf("\n");
+a++;
+ printf("Splitted string is:");
+ printf("%s",a);
+}
+void case_chg(char *s)
+{
+     while(*s!='\0')
+     {       
+       if(*s>='a' && *s<='z')
+       {*s=*s+32;
+        printf("%c",*s++);
+       }
+  if(*s>='A' && *s<='Z')
+  {*s=*s-32;
+   printf("%c",*s++);
+  }
+     }
+ void encrypt(char *s,int n)
+ {int i,j=strlen(s);
+  printf("\nThe encrypted string is\n");
+  for(i=0;i<j;i++)
+  {if(isupper(*s)||islower(*s))
+       *s=(*s+n)%26;
+   printf("%c",*s++);
+  }
+  void decrypt(char *s,int n)
+  {int i,j=strlen(s);
+   printf("\nDecrypted string is\n");
+   for(i=0;i<j;i++)
+   {if(isupper(*s)||(islower(*s))
+       *s=*s-n%26;
+       printf("%c",*s++);
+       }
+       }
+ void main()
+   {
+     char a[50],b; 
+   printf("\nEnter the string\n");
+gets(a);
+  printf("enter the char. from where string you want to be split\n);
+ scanf("%c",b);
+  split_string(&a[0],b);
+  case_chg(&a[0]);
+ printf("Enter a number\n");
+   scanf("%d%",n);
+  encrypt(&a[0],n);
+  decrypt(&a[0],n);
+         }        
+         
+ 
